779a512cf911c6b64aac172c5f8259a81851aba5
SebastianDuarte13 <jsdr137@gmail.com>
2024-08-21 19:14:01 -0500

    :ambulance: ya no aguanto mas :rotating_light:

--- src/main/java/form/Main.java
+++ src/main/java/form/Main.java
@@ -1,40 +1,18 @@
-
-
 package form;
 
-import form.chapter.application.CreateChapterUseCase;
-import form.chapter.domain.service.ChapterService;
-import form.chapter.infrastructure.in.ChapterController;
-import form.chapter.infrastructure.out.ChapterRepository;
-import form.questions.application.CreateQuestionUseCase;
-import form.questions.domain.service.QuestionService;
-import form.questions.infrastructure.in.QuestionController;
-import form.questions.infrastructure.out.QuestionRespository;
+import form.menu.Login;
 
 public class Main {
     public static void main(String[] args) {
-        // Crear las instancias necesarias
-        // ChapterService chapterService = new ChapterRepository(); // Implementación del repositorio
-        // CreateChapterUseCase createChapterUseCase = new CreateChapterUseCase(chapterService);
+
+        // Crear una instancia de la clase Login
+        Login login = new Login();
+        // Mostrar la ventana login
+        login.setVisible(true);
+
+
         
-        // // Instanciar el controlador con las dependencias
-        // ChapterController chapterController = new ChapterController(createChapterUseCase, chapterService);
-        
-        // // Ejecutar el menú CRUD
-        // chapterController.tabla_Chapter();
-
-        // Crear una instancia del repositorio de preguntas
-        QuestionService questionService = new QuestionRespository();
-
-        // Crear una instancia del caso de uso para la creación de preguntas
-        CreateQuestionUseCase createQuestionUseCase = new CreateQuestionUseCase(questionService);
-
-        // Crear una instancia del controlador de preguntas
-        QuestionController questionController = new QuestionController(createQuestionUseCase, questionService);
-
-        // Mostrar el menú de gestión de preguntas
-        questionController.tabla_Question();
-
+        // Ya una vez terminado el programa manda un mensajito
+        System.out.println("Programa terminado.");
     }
 }
-
